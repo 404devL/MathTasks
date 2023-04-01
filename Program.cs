3e26702fe828e3c8e1fce0ebe31c00840bdff1e2
Kirill Vasin <illov2564@gmail.com>
2023-04-01 18:06:12 +0300

    First modify

--- Program.cs
+++ Program.cs
@@ -8,6 +8,8 @@ namespace MathTask1
         {
             double x1, y1, x2, y2, x3, y3;
             double a, b, c, square, perimeter, p;
+
+            Console.WriteLine("Math operations.\n");
             Console.WriteLine("Введите координаты вершин треугольника");
             Console.Write("X1 = ");
             x1 = Convert.ToInt32(Console.ReadLine());
